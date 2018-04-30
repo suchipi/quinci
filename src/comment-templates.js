fc58241a6853bf02688e87115fdc4878cb3a61ad
Stephen Scott <me@suchipi.com>
2018-04-30 13:18:28 -0600

    Limit comments to 65536 characters
    This is GitHub's limit on comments.

--- src/comment-templates.js
+++ src/comment-templates.js
@@ -1,36 +1,51 @@
+const MAX_COMMENT_SIZE = 65536;
+
 module.exports = {
   running(jobName) {
     return `🕑 QuinCI is running '${jobName}'...\n`;
   },
   success(jobName, output) {
-    return (
+    const header =
       `✅ QuinCI run of job '${jobName}' passed.\n` +
       "<details>\n" +
       "<summary>Log output:</summary>\n" +
-      "\n``````\n" +
-      output.trim() +
-      "\n``````\n" +
-      "</details>"
+      "\n``````\n";
+    const body = output.trim();
+    const footer = "\n``````\n" + "</details>";
+
+    return (
+      header +
+      body.slice(-(MAX_COMMENT_SIZE - header.length - footer.length)) +
+      footer
     );
   },
   failure(jobName, output, code) {
-    return (
+    const header =
       `❌ QuinCI run of job '${jobName}' failed. Exit code was ${code}.\n` +
       "<details>\n" +
       "<summary>Log output:</summary>\n" +
-      "\n``````\n" +
-      output.trim() +
-      "\n``````\n" +
-      "</details>"
+      "\n``````\n";
+    const body = output.trim();
+    const footer = "\n``````\n" + "</details>";
+
+    return (
+      header +
+      body.slice(-(MAX_COMMENT_SIZE - header.length - footer.length)) +
+      footer
     );
   },
   error(jobName, error) {
-    return (
+    const header =
       `❌ QuinCI run of job '${jobName}' errored.\n` +
       "Error:\n" +
-      "\n``````\n" +
-      error.toString().trim() +
-      "\n``````\n"
+      "\n``````\n";
+    const body = error.toString().trim();
+    const footer = "\n``````\n" + "</details>";
+
+    return (
+      header +
+      body.slice(-(MAX_COMMENT_SIZE - header.length - footer.length)) +
+      footer
     );
   },
 };
